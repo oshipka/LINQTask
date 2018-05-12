5e811fffffb0770c01ff1103d222c0bf1e0bb2c3
oshipka <olenkashipka@gmail.com>
2018-05-12 13:16:43 +0300

    added miscelaneous functions

---
+++ LINQ/Misc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LINQ
+{
+    public class Misc
+    {
+        public static void ParseInts(List<List<int>> listName, string pathName)
+        {
+            var lines = File.ReadAllLines(pathName);
+            
+            foreach (var line in lines)
+            {
+                var dataRow = new List<int>();
+                var numbers = line.Split(' ');
+                foreach (var number in numbers)
+                {
+                    int.TryParse(number, out var result);
+                    dataRow.Add(result);
+                }
+                listName.Add(dataRow);
+            }
+        }
+
+        public static string CurrentDir()
+        {
+            var directory = Directory.GetCurrentDirectory();
+            var pos = directory.Split('\\');
+            var result = pos[0];
+            for (var i = 1; i < pos.Length - 2; i++)
+            {
+                result += "\\" + pos[i];
+            }
+
+            return result;
+        }
+
+        public static string SetPath(string fileName, bool read)
+        {
+            return Path.Combine(CurrentDir(), read ? "Data files" : "Result files", fileName);
+        }
+    }
+}
