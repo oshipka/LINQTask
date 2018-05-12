3487f08b780f408b346b8ad6df3c15da315fb54f
oshipka <olenkashipka@gmail.com>
2018-05-12 13:15:46 +0300

    completed task 16

---
+++ LINQ/Linq16.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+
+namespace LINQ
+{
+	public class Linq16
+	{
+		public static void TaskMain()
+		{
+			var readPath = Misc.SetPath("Task16.txt", true);
+			var writePath = Misc.SetPath("Task16.txt", false);
+			var taskData = new List<List<int>>();
+			Misc.ParseInts(taskData, readPath);
+			foreach (var dataLine in taskData)
+			{
+				var positive = from i in dataLine where i > 0 select i;
+				foreach (var variable in positive)
+				{
+					File.AppendAllText(writePath, variable.ToString());
+					File.AppendAllText(writePath, " ");
+				}
+				File.AppendAllText(writePath, Environment.NewLine);
+			}
+		}
+	}
+}
--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -10,6 +10,7 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
+            Linq16.TaskMain();
         }
     }
 }
